1d240823c8fc31448c243626c2f2fdccf8d0d2e8
SwapnilSoni1999 <swapnil.soni12345@gmail.com>
2021-03-27 17:01:22 +0530

    middleware: enc: handle rejection

--- middleware/encryptedJSON.js
+++ middleware/encryptedJSON.js
@@ -2,19 +2,23 @@ const cipher = require('../utils/cipher')
 
 const decryptMiddleware = async (req, res, next) => {
     if (req.headers['content-type'] === 'application/encrypted-json') {
-        const payload = req.body.toString().trim()
-        const xKey = req.headers['x-key']
-        if (!xKey) {
-            return res.status(403).json({ message: "No key provided!" })
+        try {
+            const payload = req.body.toString().trim()
+            const xKey = req.headers['x-key']
+            if (!xKey) {
+                return res.status(403).json({ message: "No key provided!" })
+            }
+            const aesKey = cipher.RSAdecrypt(xKey)
+            const jsonData = cipher.AESdecrypt(payload, aesKey)
+        
+            // verify signature: left
+            req.body = JSON.parse(jsonData)
+            return next()
+        } catch (err) {
+            return res.status(401).json({ message: "Access denied!", reason: "Invalid key!" })
         }
-        const aesKey = cipher.RSAdecrypt(xKey)
-        const jsonData = cipher.AESdecrypt(payload, aesKey)
-    
-        // verify signature: left
-        req.body = JSON.parse(jsonData)
-        next()
     } else {
-        next()
+        return next()
     }
 }
 
